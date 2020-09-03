1ce10a7b16e03f9692cde9fdc10516df066214bd
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2020-09-04 00:30:04 +0900

    Add files via upload

---
+++ 연습문제/109. 잔돈.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+char *Answer;
+
+int main()
+{
+	int T, test_case;
+	
+	scanf("%d", &T);
+	
+	for(test_case = 0; test_case < T; test_case++)
+	{
+        int coin;
+        scanf("%d",&coin);
+        
+        int a,b,c,d;
+        a=coin/500;
+        coin%=500;
+        b=coin/100;
+        coin%=100;
+        c=coin/50;
+        coin%=50;
+        d=coin/10;
+        
+        char temp[4];
+        sprintf(temp,"%d %d %d %d",d,c,b,a);
+        Answer = temp;
+        
+		printf("Case #%d\n", test_case+1);
+		printf("%s\n", Answer);
+	}
+
+	return 0;
+}
+
